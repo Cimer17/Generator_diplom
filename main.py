7f046252e552d299fd58363a6346b880d87cdb77
Cimer17 <ivanklopov2000@yandex.ru>
2023-02-02 20:24:28 +0300

    new_chanege

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import pandas as pd
-import os
-import datetime
-
-import docx
-from docx.shared import Inches, Cm
-
+from docx.shared import Cm
 from PIL import Image, ImageDraw, ImageFont
 from PyQt5 import QtCore, QtGui, QtWidgets
 from qt_material import apply_stylesheet
+import pandas as pd
+import os
+import datetime
+import docx
 
 
 class Ui_mainWindow(QtWidgets.QMainWindow):
@@ -76,83 +74,68 @@ class Ui_mainWindow(QtWidgets.QMainWindow):
     def clear_text(self):
         self.text_status.setText("Жду задания...")
 
+    def drawing(self, img, name_img):
+        draw = ImageDraw.Draw(img)
+        font_FIO = ImageFont.truetype("arial.ttf", 70, encoding='utf-8')
+        font_year = ImageFont.truetype("arial.ttf", 45, encoding='utf-8')
+        name_size = len(name_img)
+        now_year = f'{datetime.datetime.now().year} год'
+        if name_size > 25:
+            draw.text(xy=(500, 610), text=f'{name_img}', fill=(0, 0, 0), font=font_FIO)
+        elif name_size > 16:
+            draw.text(xy=(649 - name_size * 4 , 610), text=f'{name_img}', fill=(0, 0, 0), font=font_FIO)
+        else:
+            draw.text(xy=(649, 610), text=f'{name_img}', fill=(0, 0, 0), font=font_FIO)
+        draw.text(xy=(890, 1300), text=f'{now_year}', fill=(0, 0, 0), font=font_year)
+
+    def save_doc(self, imagelist, napravlenie):
+        doc = docx.Document()
+        sections = doc.sections
+        for section in sections:
+            section.top_margin = Cm(0)
+            section.bottom_margin = Cm(0)
+            section.left_margin = Cm(1)
+            section.right_margin = Cm(0)
+        for image in imagelist:
+            doc.add_picture(image, width=Cm(19.6), height=Cm(13.6))
+        doc.save(f'doc/{napravlenie}.docx')
+    
     def create_img(self):
-        
         imagelist = []
         napravlenie = self.comboBox.currentText()
         df = pd.read_csv(f"data/{napravlenie}_DB.csv")
-        
-        font_FIO = ImageFont.truetype("arial.ttf", 70, encoding='utf-8')
-        font_year = ImageFont.truetype("arial.ttf", 45, encoding='utf-8')
-        
         pathc = os.path.abspath('pictures')
         url_patch = f'{pathc}\{napravlenie}'.replace('\\', '/')
-        
-        now_year = f'{datetime.datetime.now().year} год'
-        
         if os.path.exists(url_patch):
             pass 
         else:
             os.mkdir(url_patch)
-        
         for index, j in df.iterrows():
-            
             img = Image.open(f'tmp/{napravlenie}.png')
-            draw = ImageDraw.Draw(img)
-            
             name_img = (j['name'])
-            name_size = len(name_img)
-            
-            if name_size > 25:
-                draw.text(xy=(500, 610), text=f'{name_img}', fill=(0, 0, 0), font=font_FIO)
-            elif name_size > 16:
-                draw.text(xy=(649 - name_size * 4 , 610), text=f'{name_img}', fill=(0, 0, 0), font=font_FIO)
-            else:
-                draw.text(xy=(649, 610), text=f'{name_img}', fill=(0, 0, 0), font=font_FIO)
-
-            draw.text(xy=(890, 1300), text=f'{now_year}', fill=(0, 0, 0), font=font_year)
-            
+            self.drawing(img, name_img)
             patch_name = f'pictures/{napravlenie}/{name_img}.png'.replace(' ', '_')
             img.save(patch_name) 
             imagelist.append(patch_name)
-        
-        
-        if self.status_doc == True:
-            
-            doc = docx.Document()
-            sections = doc.sections
-            for section in sections:
-                section.top_margin = Cm(0)
-                section.bottom_margin = Cm(0)
-                section.left_margin = Cm(1)
-                section.right_margin = Cm(0)
-            
-            for image in imagelist:
-                doc.add_picture(image, width=Cm(19.6), height=Cm(13.6))
-            
-
-            doc.save(f'doc/{napravlenie}.docx')
-        
+        if self.status_doc == True: 
+            self.save_doc(imagelist, napravlenie)
         self.text_status.setText("Готово!")
 
-
-if __name__ == "__main__":
-    
+def main():
     import sys
-    
     check = os.path.exists('pictures')
     if check == False:
         os.mkdir("pictures")
     check = os.path.exists('doc')
     if check == False:
         os.mkdir("doc")
-    
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = QtWidgets.QMainWindow()
     apply_stylesheet(app, theme='dark_blue.xml')
-    
     ui = Ui_mainWindow()
     ui.setupUi(mainWindow)
-    
     mainWindow.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
